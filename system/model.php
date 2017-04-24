12530e2996926cc2a0d5670323dbb5a8e984f5e9
taciobrito <tacio.7brito@gmail.com>
2017-04-24 17:51:58 -0300

    Model

--- system/model.php
+++ system/model.php
@@ -7,38 +7,40 @@ class Model{
 	public $table;
 
 	public function __construct(){
-		require_once( __DIR__ . '/../app/config/database.php' );
-		$this->db = new PDO( 'mysql:host=' . $db['hostname'] . ';dbname=' . $db['database'], $db['username'], $db['password'] );
+		require (__DIR__ . '/../app/config/database.php');
+		$this->db = new PDO('mysql:host=' . $db['hostname'] . ';dbname=' . $db['database'], $db['username'], $db['password']);
 	}
 	
-	public function create( Array $data ){
-		$fields = implode(", ", array_keys($data));
-		$values = "'" . implode("','", array_values($data))."'";
-		return $this->db->query( "INSERT INTO `{$this->table}` ({$fields}) VALUES ({$values})" );
+	public function criar( Array $dados ){
+		$campos = implode(", ", array_keys($dados));
+		$valores = "'" . implode("','", array_values($dados))."'";
+		return $this->db->query( "INSERT INTO {$this->_tabela} ({$campos}) VALUES ({$valores})" );
 	}
 	
-	public function listar( $where = null, $limit = null, $offset = null, $orderby = null ){
-		$where = ($where != null ? "WHERE {$where}" : "");
-		$limit = ($limit != null ? "LIMIT {$limit}" : "");
-		$offset = ($offset != null ? "OFFSET {$offset}" : "");
-		$orderby = ($orderby != null ? "ORDER BY {$orderby}" : "");
+	public function listar( Array $select = null ){
+		$fields = ( isset($select['fields']) ? "{$select['fields']}" : "*");
+		$join = ( isset($select['join']) ? "{$select['join']}" : "");
+		$where = ( isset($select['where']) ? "WHERE {$select['where']}" : "");
+		$limit = ( isset($select['limit']) ? "LIMIT {$select['limit']}" : "");
+		$offset = ( isset($select['offset']) ? "OFFSET {$select['offset']}" : "");
+		$orderby = ( isset($select['orderby']) ? "ORDER BY {$select['orderby']}" : "");
 		
-		$sql = $this->db->prepare( "SELECT * FROM `{$this->table}` {$where} {$orderby} {$limit} {$offset}" );
+		$sql = $this->db->prepare( "SELECT {$fields} FROM {$this->_tabela} {$join} {$where} {$orderby} {$limit} {$offset}" );
 		$sql->execute();
 		$sql->setFetchMode(PDO::FETCH_ASSOC);
 		return $sql->fetchAll();
 	}
 	
-	public function atualizar( Array $data, $where ){
-		foreach ( $data as $indice => $valor){
-			$fields[] = "{$indice} = '{$valor}'";
+	public function atualizar( Array $dados, $where ){
+		foreach ( $dados as $indice => $valor){
+			$campos[] = "{$indice} = '{$valor}'";
 		}
-		$fields = implode(", ", $fields);
-		$this->db->query( "UPDATE `{$this->table}` SET {$fields} WHERE {$where}" );
-		return $fields;
+		$campos = implode(", ", $campos);
+		$this->db->query( "UPDATE {$this->_tabela} SET {$campos} WHERE {$where}" );
+		return $campos;
 	}
 	
 	public function deletar( $where ){
-		return $this->db->query( "DELETE FROM `{$this->table}` WHERE {$where}" );
+		return $this->db->query( "DELETE FROM {$this->_tabela} WHERE {$where}" );
 	}
 }
