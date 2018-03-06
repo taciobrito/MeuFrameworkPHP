e394cfad1f457b60b8f71d42f0369ad3eb4069fb
taciobrito <tacio.7brito@gmail.com>
2018-03-06 17:27:45 -0300

    ...Teste...

--- app/view/index.php
+++ app/view/index.php
@@ -13,8 +13,39 @@
 			<h1><?php echo $hello; ?></h1>
 			<p><?php echo $message; ?></p>
 
+			<br><br><br>
+			<button id="env">Enviar</button>
+
 		</div>
 
+		<script src="<?php echo $this->url('assets/js/jquery.min.js'); ?>"></script>
+
+		<script>
+			$('#env').click(function(event) {
+					$.ajax({
+						url: 'http://localhost/app_personal/backend/service/postPersonal',
+						type: 'POST',
+						dataType: 'json',
+						data: {
+							// id_users: '',
+							cpf: '06062285195',
+							password: '211540',
+							name: 'Algum nome que eu quis CKM',
+							birth_date: '2000-03-01',
+							Email: 'email.algum@mail.com',
+							cel: '(61)22223365',
+							cep: '72545-500',
+							neighborhood: 'QR 315 H',
+							address: 'QUADRA DE SANTA MARIA',
+							cref: '22223365',
+						},
+						success: function(d) {
+							console.log(d);
+							console.log("success");
+						},
+					});
+				});
+		</script>
 	</body>
 
 </html>
